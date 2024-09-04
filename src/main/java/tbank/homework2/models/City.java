a3bf120e23fcf406bc16f2273914079e91b7c0e2
Эльвина Фаттахова <99656190+elvinaf1996@users.noreply.github.com>
2024-09-04 19:01:53 +0300

    добавила геттеры, сеттеры и конструкторы в моделях

--- src/main/java/tbank/homework2/models/City.java
+++ src/main/java/tbank/homework2/models/City.java
@@ -1,6 +1,7 @@
 package tbank.homework2.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,6 +9,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 public class City implements JsonModel {
 
     @JsonProperty("slug")
--- src/main/java/tbank/homework2/models/Coordinates.java
+++ src/main/java/tbank/homework2/models/Coordinates.java
@@ -1,9 +1,15 @@
 package tbank.homework2.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+@Getter
+@Setter
 @ToString
+@AllArgsConstructor
 public class Coordinates implements JsonModel {
 
     @JsonProperty("lat")
